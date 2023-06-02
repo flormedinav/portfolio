ad6035cbe5cd9c1d886eb4685aec3846f3a8aefe
Florencia Medina <110070980+flormedinav@users.noreply.github.com>
2023-06-01 23:38:37 -0300

    Se modificaron las imagenes de projectos

--- client/src/components/SectionProjects/projects.js
+++ client/src/components/SectionProjects/projects.js
@@ -1,13 +1,15 @@
 import imageDogsApp from "../../assets/dogsApp.png";
 import imageRickAndMorty from "../../assets/rickAndMorty.png";
 import imagePF from "../../assets/pf.PNG";
-import imageEloleros from "../../assets/dogsApp.png";
+import imageEloleros from "../../assets/inicioEloleros.PNG";
 import imageChocolate from "../../assets/chocolate.jpg";
+import imagePortfolio from "../../assets/inicioPortfolio.PNG";
+import imageIPhone from "../../assets/inicioIPhone.PNG";
 
 export const projects = [
   {
     name: "Portfolio",
-    img: imageEloleros,
+    img: imagePortfolio,
     description: "Porfolio personal",
     github: "https://github.com/flormedinav/Proyecto-Individual-Dogs",
   },
@@ -18,26 +20,26 @@ export const projects = [
     github: "https://github.com/flormedinav/Proyecto-Individual-Dogs",
   },
   {
+    name: "Dogs App",
+    img: imageDogsApp,
+    description: "Página web de razas de perros.",
+    github: "https://github.com/flormedinav/Proyecto-Individual-Dogs",
+  },
+  {
+    name: "Rick And Morty",
+    img: imageRickAndMorty,
+    description: "Página de la serie de Rick and Morty.",
+    github: "https://github.com/flormedinav/Proyecto-Individual-Dogs",
+  },
+  {
     name: "The Chocolate Hub",
     img: imageChocolate,
     description: "E-commerce de chocolates.",
     github: "https://github.com/flormedinav/Proyecto-Individual-Dogs",
   },
   {
-    name: "Dogs App",
-    img: imageRickAndMorty,
-    description: "Página web de razas de perros.",
-    github: "https://github.com/flormedinav/Proyecto-Individual-Dogs",
-  },
-  {
-    name: "Rick And Morty",
-    img: imageEloleros,
-    description: "Página de la serie de Rick and Morty.",
-    github: "https://github.com/flormedinav/Proyecto-Individual-Dogs",
-  },
-  {
     name: "iPhone 13",
-    img: imageEloleros,
+    img: imageIPhone,
     description: "Página web sobre iPhone 13",
     github: "https://github.com/flormedinav/Proyecto-Individual-Dogs",
   },
